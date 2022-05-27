a780c3f45922437a00d7dadf21df8b9a3634e289
delinuxist <delinuxist@gmail.com>
2022-05-27 16:07:14 +0000

    register feature working

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,28 @@
+require("dotenv").config();
+const { StatusCodes } = require("http-status-codes");
+const { BadRequest } = require("../errors");
+const User = require("../models/user.model");
+
 exports.register = async (req, res) => {
-  res.send("register");
+  const { email } = req.body;
+
+  const user = await User.findOne({ email: email });
+
+  // check if email already exists
+  if (user) {
+    throw new BadRequest(`User with email: ${email} already exits`);
+  }
+
+  const newUser = await User.create({ ...req.body });
+
+  const token = await newUser.createJwt();
+
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: newUser.getName(),
+    },
+    token,
+  });
 };
 
 exports.login = async (req, res) => {
